4091ef149bd2543873df8dfcf8d1ac064c969a5e
Takashi Kawase <kawaset@kawaset-ws1.int.janelia.org>
2018-09-12 13:34:59 -0400

    modify unit test

--- src/test/java/org/janelia/it/h5j/fiji/adapter/FijiAdapterTest.java
+++ src/test/java/org/janelia/it/h5j/fiji/adapter/FijiAdapterTest.java
@@ -60,6 +60,12 @@ public class FijiAdapterTest {
     @Test
     public void getImagePlus() throws Exception {
         ImagePlus imagePlus = fijiAdapter.getMultiChannelImagePlus(new File(TESTFILE));
+        if (imagePlus == null) System.out.println("getMultiChannelImagePlus failed (return value is NULL)");
+        else {
+        	System.out.println("bpp: "+imagePlus.getBytesPerPixel());
+        	System.out.println("height: "+imagePlus.getHeight());
+            System.out.println("width: "+imagePlus.getWidth());
+        }
         assertEquals("Bytes-per-pixel fails to match", EXPECT_BPP, imagePlus.getBytesPerPixel());
         assertEquals("Height not as expected", EXPECT_HEIGHT, imagePlus.getHeight());
         assertEquals("Width not as expected", EXPECT_WIDTH, imagePlus.getWidth());
