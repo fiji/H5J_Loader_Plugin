d026a1bb3ce83a8a6dbb1057294fa847379b4cae
Curtis Rueden <ctrueden@wisc.edu>
2022-08-26 15:41:54 -0500

    CI: install libxcb-shape0
    Apparently it is required by FFMPEG's libjniavdevice.

--- .github/setup.sh
+++ .github/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-setup-github-actions.sh
 sh ci-setup-github-actions.sh
+
+# NB: Needed to avoid CI test failure: java.lang.UnsatisfiedLinkError: /home/runner/.javacpp/cache/ffmpeg-4.2.1-1.5.2-linux-x86_64.jar/org/bytedeco/ffmpeg/linux-x86_64/libjniavdevice.so: libxcb-shape.so.0: cannot open shared object file: No such file or directory
+sudo apt-get -y install libxcb-shape0
